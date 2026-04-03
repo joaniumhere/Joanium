ed89dde90630ca3d6e25691490356773cfbf402a
Joel Jolly <128782382+withinJoel@users.noreply.github.com>
2026-04-03 13:15:05 +0530

    about version missing bug fix

--- Packages/Main/IPC/SystemIPC.js
+++ Packages/Main/IPC/SystemIPC.js
@@ -1,10 +1,12 @@
-import { ipcMain } from 'electron';
+import { ipcMain, app } from 'electron';
 import * as UserService from '../Services/UserService.js';
 import * as SystemPromptService from '../Services/SystemPromptService.js';
 import Paths from '../Core/Paths.js';
 
 export const ipcMeta = { needs: ['connectorEngine', 'featureRegistry'] };
 export function register(connectorEngine, featureRegistry = null) {
+  ipcMain.handle('get-app-version', () => app.getVersion());
+
   ipcMain.handle('get-system-prompt', async () => {
     try {
       return await SystemPromptService.get({
